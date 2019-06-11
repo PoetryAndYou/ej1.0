b955559ae5bed861df491ce25ba2697616e78255
gujunling <gujunq1998@163.com>
2019-06-11 18:05:17 +0800

    customer实现基本的查询所有

--- src/main/java/com/briup/apps/ej/service/ICustomerService.java
+++ src/main/java/com/briup/apps/ej/service/ICustomerService.java
@@ -1,8 +1,13 @@
 package com.briup.apps.ej.service;
 
+import com.briup.apps.ej.bean.Customer;
+
+import java.util.List;
+
 /**
  * @author rui
  * @create 2019-06-10 11:26
  */
 public interface ICustomerService {
+    List<Customer> findAll();
 }
