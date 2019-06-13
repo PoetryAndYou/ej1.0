e086e53f5354811edfedc4b035e23a0faf9725c6
ZBG <zbgguo@qq.com>
2019-06-13 16:17:23 +0800

    waiter代码修改优化

--- src/main/java/com/briup/apps/ej/web/controller/WaiterController.java
+++ src/main/java/com/briup/apps/ej/web/controller/WaiterController.java
@@ -18,7 +18,7 @@ import java.util. List;
  * @create 2019-06-10 13:43
  */
 @RestController
-@RequestMapping("/ waiter")
+@RequestMapping("/waiter")
 
 public class WaiterController {
     @Autowired
