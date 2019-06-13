9f896379470a5d4337ed408d8ad176da3f76e752
ZBG <zbgguo@qq.com>
2019-06-13 08:46:00 +0800

    员工增加批量删除功能

--- src/main/java/com/briup/apps/ej/web/controller/WaiterController.java
+++ src/main/java/com/briup/apps/ej/web/controller/WaiterController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
 import java.nio.file.WatchService;
-import java.util.List;
+import java.util. List;
 
 /**
  * @author zhangbaoguo
