540ac92864950bdafa6654d2a6f55843e86a7b72
ychengrui@163.com <ychengrui@163.com>
2019-06-18 16:42:34 +0800

    product模糊查询

--- src/main/java/com/briup/apps/ej/web/controller/ProductController.java
+++ src/main/java/com/briup/apps/ej/web/controller/ProductController.java
@@ -67,7 +67,7 @@ public class ProductController {
         return MessageUtil.message("删除成功");
     }
     @ApiOperation("模糊查询")
-    @PostMapping("query")
+    @GetMapping("query")
     public Message query(Product product){
         List<Product> list=productService.query(product);
         return MessageUtil.success("查询成功",list);
